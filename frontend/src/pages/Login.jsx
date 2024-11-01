59a6cdf9a0e3c6704ba41ef711dd63d1d7ee854b
emmspy <emilysky48@gmail.com>
2024-11-01 16:00:45 -0500

    Login listo y profile

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -14,22 +14,37 @@ const onSubmitHandler = (e)=> {
   return (
     <div>
       <form action="" className='min-h-[80vh] flex flex-col item-center'>
-        <div className='flex flex-col gap-3 m-auto items-start p-8 min-w-[340px] sm:min-w-96 border rounded-xl text-zinc-600'>
-          <p> {changelog === 'Sign up' ? 'Registrarse' : 'Login'} </p>
-          <p> {changelog === 'Sign up' ? 'Registrate' : 'si ya tienes cuenta, ingresa'} y disfruta de todas nuestras clases</p>
-          <div className='flex flex-col'>
+        <div className='flex flex-col gap-3 m-auto items-start p-8 min-w-[340px] sm:min-w-96 border rounded-xl text-primary-green shadow-lg'>
+          <p className='text-2xl my-4 font-semibold'> {changelog === 'Sign up' ? 'Registrarse' : 'Login'} </p>
+          <p className='font-light my-3 italic'> {changelog === 'Sign up' ? 'Registrate' : 'Si ya tienes cuenta, ingresa'} y disfruta de todas nuestras clases</p>
+          
+          {
+            changelog === 'Sign up'
+            
+          ?   
+            
+          <div className='flex flex-col w-full'>
             <label >Nombre</label>
-            <input type="text" onChange={(e)=>setName(e.target.name)} value={name} required/>
+            <input className='border-b border-zinc-300 w-full p-2 mt-1 mb-2' type="text" onChange={(e)=>setName(e.target.name)} value={name} required/>
           </div>
-          <div className='flex flex-col'>
+
+          : ''
+          
+          }
+          <div className='flex flex-col w-full'>
             <label >Email</label>
-            <input type="email" onChange={(e)=>setName(e.target.name)} value={name} required/>
+            <input className='border-b border-zinc-300 w-full p-2 mt-1 mb-2' type="email" onChange={(e)=>setEmail(e.target.name)} value={email} required/>
           </div>
-          <div className='flex flex-col'>
+          <div className='flex flex-col w-full'>
             <label >Contraseña</label>
-            <input type="text" onChange={(e)=>setName(e.target.name)} value={name} required/>
+            <input className='border-b border-zinc-300 w-full p-2 mt-1 mb-4' type="text" onChange={(e)=>setPass(e.target.name)} value={pass} required/>
           </div>
-          <button>{changelog ==='Sing up' ? 'Crear cuenta' : 'Iniciar sesion'}</button>
+          <button className='bg-primary-green p-3 rounded-full  text-primary-cream'>{changelog ==='Sing up' ? 'Crear cuenta' : 'Iniciar sesion'}</button>
+          {
+            changelog === 'Sign up'
+            ? <p className='font-light my-3 italic'>Ya tienes una cuenta? <span className='font-semibold' onClick={()=> setChangelog('login')}> Registrase </span></p>
+            : <p className='font-light my-3 italic' >Aun no tienes una cuenta? <span className='font-semibold'  onClick={()=> setChangelog('Sign up')} >Crear cuenta</span></p>
+          }
         </div>
       </form>
     </div>
--- frontend/src/pages/MyProfile.jsx
+++ frontend/src/pages/MyProfile.jsx
@@ -1,9 +1,29 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { assets } from '../assets/assets'
 
 const MyProfile = () => {
+
+  const [userData, setUserData] = useState({
+    name: 'Robert Patinson',
+    image: assets.user_picture,
+    email: 'ejemplo@gmail.com',
+    phone: '+1234567',
+    address: 'calle 136 #101b -24',
+    dob: '11-08-2003',
+
+  })
+
+  const [isEdit, setIsEdit] = useState(false)
+
   return (
     <div>
-      ver mi perfil
+      <img src={userData.image} alt="" />
+      {
+        isEdit 
+        ? <input type="text" />
+        : <p>{userData.name}</p>
+      }
+
     </div>
   )
 }
